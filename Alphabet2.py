bc72608ce83919792638a6522abfab0bce4ab4ce
nishant5254 <kumarnishant52541@gmail.com>
2019-02-21 17:20:45 +0530

    File commited

---
+++ Alphabet2.py
@@ -0,0 +1,9 @@
+Number_rows=int(input("Enter the no of rows you want to print: "))
+Number=65
+for i in range(0,Number_rows):
+   for j in range(0,i+1):
+       ch=chr(Number)
+       print(ch,end=" ")
+       Number=Number+1
+   Number=65    
+   print("\n") 
