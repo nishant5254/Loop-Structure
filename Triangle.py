681da27e0aa2f51b3b360a0a9546d77cd5b2374c
nishant5254 <kumarnishant52541@gmail.com>
2019-02-21 17:23:39 +0530

    File commited

---
+++ Triangle.py
@@ -0,0 +1,10 @@
+Number_rows=int(input("Enter no of rows you want print: "))
+k=2*Number_rows #number of spaces
+for i in range(0,Number_rows):
+    for j in range(0,k):
+
+        print(end=" ")
+    k=k-1   
+    for j in range(0,i+1):
+            print("*",end=" ")       
+    print("\n")
