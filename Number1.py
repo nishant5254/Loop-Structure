9885d65ee7c18e40abe494fdf595e03be2b600ae
nishant5254 <kumarnishant52541@gmail.com>
2019-02-21 17:21:39 +0530

    File commited

---
+++ Number1.py
@@ -0,0 +1,28 @@
+Number_rows=int(input("Enter the no of rows you want to print: "))
+for i in range(0,Number_rows):
+
+    Number=1
+    for j in range(0,i+1):
+        print(Number,end=" ")
+        Number=Number+1
+
+    print("\n")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1
+12
+123
+1234
+12345
